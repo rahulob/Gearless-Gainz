4df86559f7d1c21557b53fdcffc8c9cda6f689f6
Rahul Gupta <rahuljandial2@gmail.com>
2024-08-13 15:49:32 +0530

    Initial Commit

---
+++ Gearless Gainz/Gearless_GainzApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gearless_GainzApp.swift
+//  Gearless Gainz
+//
+//  Created by Rahul Gupta on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Gearless_GainzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
